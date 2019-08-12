766e93189f73fbabe8e537c8a0697e20f31203fc
Jason Chen <Jason.Chen@CODE5600.NRL.NAVY.MIL>
2019-08-12 11:26:14 -0400

    Revised and rewritten pressure drop MATLAB code

---
+++ pressureLossCalc_OLD.m
@@ -0,0 +1,78 @@
+figure;
+count = 1;
+x=[];
+x2=[];
+y=[];
+for i=.125:.01:.6
+    x(count)=i;
+    y(count)=getDataDiam(i);
+    count=count+1;
+end
+disp(getDataDiam(12.7));
+disp(x);
+disp(y);
+plot(x,y);
+
+title('Effect of Pipe Diameter on Pressure Loss');
+xlabel('Pipe Diameter (in)');
+ylabel('Pressure Drop (mPa)');
+
+figure;
+count = 1;
+x3=[];
+x4=[];
+y3=[];
+for i=1:1:40
+    x3(count)=i;
+    y3(count)=getDataRough(i);
+    x4(count)=i;
+    count=count+1;
+end
+disp(x3);
+disp(y3);
+plot(x3,y3);
+
+title('Effect of Pipe Roughness on Pressure Loss');
+x3label('Pipe Roughness (microm)');
+y3label('Pressure Drop (mPa)');
+
+function data = getDataDiam(diameter)
+    mdot= 0.9133;
+    density= 786;
+    visco = 0.00196;
+    length = 8;
+    diam = diameter*2.54;
+    rough = .0000015;
+    g = 9.8;
+    
+    Q = mdot/density;
+    V = Q/(pi*(diam/2)^2);
+    kvisc = visco/density;
+    red = V*diam/kvisc;
+    
+    f = (1/(-1.8*log10((6.9/red)+((rough/diam)/3.7)^1.11)))^2;
+    hf = f*(length/diam)*(V^2/(2*g));
+    dP = hf*density*g; 
+    data = dP/1000000;
+end
+
+function data2 = getDataRough(roughness)
+    mdot= 0.9133;
+    density= 786;
+    visco = 0.00196;
+    length = 8;
+    diam = 0.0127;
+    g = 9.8;
+    
+    rough = roughness / 10^-6;
+    
+    Q = mdot/density;
+    V = Q/(pi*(diam/2)^2);
+    kvisc = visco/density;
+    red = V*diam/kvisc;
+    
+    f = (1/(-1.8*log10((6.9/red)+((rough/diam)/3.7)^1.11)))^2;
+    hf = f*(length/diam)*(V^2/(2*g));
+    dP = hf*density*g;
+    data2 = dP/1000000;
+end
