d9e1665f4ac6a67153e07f54ff105ff138888252
JayChen35 <jay.chen135@gmail.com>
2019-09-01 14:59:51 -0400

    Add simple isentropic calculation file

---
+++ tankPressureSimple.m
@@ -0,0 +1,10 @@
+%% God help Huzel & Huang R.I.P @95 years old
+% const = 2116.216623671; % one atmosphere in psf 
+% totalpsf = 23760 - const;
+% totalpa = 1036320.325372; % in pascals
+% totalvol = 3.3697; %m^3
+% R = 2077.1;
+% temp = 192.222;
+% weight = (totalpa * totalvol)/(R*temp);
+% disp(weight);
+%% 
