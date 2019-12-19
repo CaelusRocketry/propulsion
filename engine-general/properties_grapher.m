88f855da7c2a4acdfc1b2b580862350c2b446c35
JayChen35 <jay.chen135@gmail.com>
2019-12-19 16:00:53 -0500

    Add updated propellant figures

--- engine-general/properties_grapher.m
+++ engine-general/properties_grapher.m
@@ -22,16 +22,18 @@ for i=0:size(data,1)-1
     x(mod(i, dims)+1) = data{i+1,2}; 
     y(mod(i, dims)+1) = data{i+1,7}; 
 end
-% yyaxis left;
-l = legend('show', 'Location', 'southeast');
-grid on;
+plot(x, y, 'DisplayName', sprintf('%.2f', current_mr));
 hold off;
+% yyaxis left;
+lgd = legend('show', 'Location', 'southeast');
+lgd.Title.String = 'Mixture Ratios';
+grid on;
 xlabel("Chamber Pressure (Bar)");
-ylabel("Theoretical C*(m/sec)");
+ylabel("Theoretical C* (m/sec)");
 title({"{\bf\fontsize{14} Chamber Pressure v. C* (Equilibrium)}" ...
     '\fontsize{10} 75% C_{2}H_{5}OH + 25% H_{2}O/Gaseous O_{2} at 5km'}, ...
     'FontWeight', 'Normal');
-% saveas(gcf, 'cp_cstar_mix.png'); % gcf = current figure
+saveas(gcf, 'cp_cstar_mix.fig'); % gcf = current figure
 
 %% Chamber Pressure v. Combustion Temperature v. Mixture Ratio
 figure;
@@ -49,16 +51,18 @@ for i=0:size(data,1)-1
     x(mod(i, dims)+1) = data{i+1,2}; 
     y(mod(i, dims)+1) = data{i+1,4}; 
 end
-% yyaxis left;
-l = legend('show', 'Location', 'southeast');
-grid on;
+plot(x, y, 'DisplayName', sprintf('%.2f', current_mr));
 hold off;
+% yyaxis left;
+lgd = legend('show', 'Location', 'southeast');
+lgd.Title.String = 'Mixture Ratios';
+grid on;
 xlabel("Chamber Pressure (Bar)");
 ylabel("Combustion Temperature (K)");
 title({"{\bf\fontsize{14} Chamber Pressure v. Temperature (Equilibrium)}" ...
     '\fontsize{10} 75% C_{2}H_{5}OH + 25% H_{2}O/Gaseous O_{2} at 5km'}, ...
     'FontWeight', 'Normal');
-% saveas(gcf, 'cp_temp_mix.png');
+saveas(gcf, 'cp_temp_mix.fig');
 
 %% Chamber Pressure v. Molar Weight v. Mixture Ratio
 figure;
@@ -76,16 +80,18 @@ for i=0:size(data,1)-1
     x(mod(i, dims)+1) = data{i+1,2}; 
     y(mod(i, dims)+1) = data{i+1,5}; 
 end
-% yyaxis left;
-l = legend('show', 'Location', 'southeast');
-grid on;
+plot(x, y, 'DisplayName', sprintf('%.2f', current_mr));
 hold off;
+% yyaxis left;
+lgd = legend('show', 'Location', 'southeast');
+lgd.Title.String = 'Mixture Ratios';
+grid on;
 xlabel("Chamber Pressure (Bar)");
 ylabel("Exhaust Molecular Weight (kg/mol)");
 title({"{\bf\fontsize{14} Chamber Pressure v. Molar Weight (Equilibrium)}" ...
     '\fontsize{10} 75% C_{2}H_{5}OH + 25% H_{2}O/Gaseous O_{2} at 5km'}, ...
     'FontWeight', 'Normal');
-% saveas(gcf, 'cp_molar_mix.png');
+saveas(gcf, 'cp_molar_mix.fig');
 
 %% Chamber Pressure v. Specific Heat Ratio v. Mixture Ratio
 figure;
@@ -103,16 +109,18 @@ for i=0:size(data,1)-1
     x(mod(i, dims)+1) = data{i+1,2}; 
     y(mod(i, dims)+1) = data{i+1,6}; 
 end
-% yyaxis left;
-l = legend('show', 'Location', 'southeast');
-grid on;
+plot(x, y, 'DisplayName', sprintf('%.2f', current_mr));
 hold off;
+% yyaxis left;
+lgd = legend('show', 'Location', 'southeast');
+lgd.Title.String = 'Mixture Ratios';
+grid on;
 xlabel("Chamber Pressure (Bar)");
 ylabel("Ratio of Specific Heats");
 title({"{\bf\fontsize{14} Chamber Pressure v. Specific Heat Ratio (Equilibrium)}" ...
     '\fontsize{10} 75% C_{2}H_{5}OH + 25% H_{2}O/Gaseous O_{2} at 5km'}, ...
     'FontWeight', 'Normal');
-% saveas(gcf, 'cp_gamma_mix.png');
+saveas(gcf, 'cp_gamma_mix.fig');
 
 %% Functions
 function [dims, num_mr] = getNumMixtureRatios(data)
