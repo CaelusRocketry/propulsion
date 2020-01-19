ec29a9977327953cf37bd64fdc687ba5c92cd6f4
Ron <gokepler@gmail.com>
2020-01-19 13:37:27 -0500

    Initial Tank Pressure Calculations

--- fluid-flow-calculations/SystemPressureCharacterization.m
+++ fluid-flow-calculations/SystemPressureCharacterization.m
@@ -31,6 +31,7 @@ for i = 1:length(pipeDiameter)
     plot3(x(start:finish), y(start:finish), fluidVelocity(start:finish));
     hold on;
 end
+hold off;
 
 %% Pressure Calculation Parameters
 solenoidCV = 2.8;               % Range: 2-4 1/2": 2.8, 3/4": 2.8
@@ -40,6 +41,7 @@ cavitatingVenturiPercent = 1.3; % Range: 1.15-1.30 (15-30%)
 chamberPressure = linspace(1.5, 3, 16);
 
 %% Tank Pressure -> Chamber Pressure Calculations and Display
+minTankPressure = zeros(1, length(chamberPressure));
 for P=1:length(chamberPressure)
     chPress = chamberPressure(P);                                           % Get chamber pressure
     endPressure = chPress .* 145.037737797;                                 % Converted chamber pressure to psi
@@ -48,9 +50,36 @@ for P=1:length(chamberPressure)
     preVenturiPressure = cavitatingVenturiPercent .* preCheckValvePressure; % Calculate needed inlet venturi pressure
     ballValvePressureDrop = pressDrop(specificGravity, 0.75, ballValveCV);  % Calculate ball valve pressure drop
     preBallValvePressure = preVenturiPressure + ballValvePressureDrop;      % Add pressure drop across ball valve
-    disp(endPressure + " psi (" + chPress + " mpa) Chamber    |   " + preBallValvePressure + " psi (" + preBallValvePressure * 145.037737797 + "mpa) Tank   |");
+    disp("Chamber Pressure: " + endPressure + " psi (" + chPress + " mpa)    |   Minimum Tank Pressure: " + preBallValvePressure + " psi (" + preBallValvePressure / 145.037737797 + "mpa)|");
+    minTankPressure(P) = preBallValvePressure;
 end
 
+%% Initial Tank Pressure (Isentropic Expansion) Visualization
+percentWater = linspace(0.3, 0.75, 10);
+initialPressure = zeros(1, length(percentWater) .* length(chamberPressure));
+x2 = zeros(1, length(percentWater) .* length(chamberPressure));
+y2 = zeros(1, length(percentWater) .* length(chamberPressure));
+i = 1;
+for P = 1:length(chamberPressure)
+    for W = 1:length(percentWater)
+        x2(i) = chamberPressure(P) .* 145.037737797;
+        y2(i) = percentWater(W);
+        initialPressure(i) = minTankPressure(P) / ((1 - percentWater(W))^1.4);
+        i = i + 1;
+    end
+end
+scatter3(x2, y2, initialPressure);
+xlabel('Chamber Pressure (psi)');
+ylabel('Percent Water in Tank (%)');
+zlabel('Initial Tank Pressure (psi)');
+title('Inital Tank Pressure as a Function of Chamber Pressure and % Water');
+hold on;
+for i = 1:length(chamberPressure)
+    start = (i-1)*length(percentWater) + 1;
+    finish = start + length(percentWater) - 1;
+    plot3(x2(start:finish), y2(start:finish), initialPressure(start:finish));
+    hold on;
+end
 %% Functions
 function fluidVelocity = velocity(pipeDiameter, volumetricFlowRate)
     fluidVelocity = (0.408 * volumetricFlowRate)/(pipeDiameter^2);
